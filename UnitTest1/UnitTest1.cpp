148867e7c675576b6ca40ec57bedf7379b934f1f
Yura <urijkisil2007@gmail.com>
2024-10-04 14:13:30 +0300

    master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_5_5/lab_5_5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(Bin1)
+        {
+            int number = 10000;
+            string binaryRepresentation = Bin(number); // Capture the result
+            Assert::AreEqual(binaryRepresentation.c_str(), "10011100010000"); // Correct expected output without spaces
+        }
+
+        TEST_METHOD(Oct1)
+        {
+            int number = 10000;
+            string octalRepresentation = Oct(number); // Capture the result
+            Assert::AreEqual(octalRepresentation.c_str(), "23420"); // Correct expected output without spaces
+        }
+	};
+}
---
+++ lab_5_5/lab_5_5.cpp
@@ -0,0 +1,150 @@
+﻿// lab_5_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int level = 0;
+int level2 = 0;
+
+string Bin(int number) {
+    string binaryRepresentation = "";
+    if (number == 0) {
+        return "";
+    }
+    else {
+        if (number % 2 == 0) {
+            binaryRepresentation = "0" + binaryRepresentation;
+        }
+        else {
+            binaryRepresentation = "1" + binaryRepresentation;
+        }
+    }
+    level++;
+    return Bin(number / 2) + binaryRepresentation;
+}
+
+string Oct(int number) {
+    string octalRepresentation = "";
+    if (number == 0) {
+        return "";
+    }
+    else {
+        if (number % 8 == 0) {
+            octalRepresentation = "0" + octalRepresentation;
+        }
+        else if (number % 8 == 1) {
+            octalRepresentation = "1" + octalRepresentation;
+        }
+        else if (number % 8 == 2) {
+            octalRepresentation = "2" + octalRepresentation;
+        }
+        else if (number % 8 == 3) {
+            octalRepresentation = "3" + octalRepresentation;
+        }
+        else if (number % 8 == 4) {
+            octalRepresentation = "4" + octalRepresentation;
+        }
+        else if (number % 8 == 5) {
+            octalRepresentation = "5" + octalRepresentation;
+        }
+        else if (number % 8 == 6) {
+            octalRepresentation = "6" + octalRepresentation;
+        }
+        else if (number % 8 == 7) {
+            octalRepresentation = "7" + octalRepresentation;
+        }
+    }
+    level2++;
+    return Oct(number / 8) + octalRepresentation; 
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number >= 0) {
+        string binaryRepresentation = Bin(number);
+        string octalRepresentation = Oct(number);
+
+        if (number == 0) {
+            cout << "Binary representation: 0" << endl;
+            cout << "Number of actions: 0" << endl;
+            cout << "Octal representation: 0" << endl;
+            cout << "Number of actions: 0" << endl;
+        }
+        else {
+            cout << "Binary representation: " << binaryRepresentation << endl;
+            cout << "Number of actions: " << level << endl;
+
+            cout << "Octal representation: " << octalRepresentation << endl;
+            cout << "Number of actions: " << level2 << endl;
+        }
+    }
+    else { cout << "Please enter >=0" << endl;}
+
+    return 0;
+}
+
+
+
+
+
+
+/*
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int level = 0;
+int level2 = 0;
+string Bin(int number) {
+    if (number == 0) {
+        return "";
+    }
+
+    level++;
+    return Bin(number / 2) + to_string(number % 2);
+}
+
+string Oct(int number) {
+    if (number == 0) {
+        return "";
+    }
+
+    level2++;
+    return Oct(number / 8) + to_string(number % 8);
+}
+
+int main() {
+    int number;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if (number >= 0) {
+        string binaryRepresentation = Bin(number);
+        string octalRepresentation = Oct(number);
+
+
+        if (number == 0) {
+            binaryRepresentation = "0";
+            octalRepresentation = "0";
+        }
+
+        cout << "Binary representation: " << binaryRepresentation << endl;
+        cout << "Number of actions: " << level << endl;
+        cout << "Octal representation: " << octalRepresentation << endl;
+        cout << "Number of actions: " << level2 << endl;
+    }
+    else {
+        cout << "Please enter a non-negative number." << endl;
+    }
+
+    return 0;
+}
+
+*/
